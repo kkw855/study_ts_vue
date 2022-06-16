1bfcae6919e93aad232206e3198ef98add150a59
kkw855 <kkw855@gmail.com>
2022-06-16 16:45:04 +0900

    Markdown 개발 중...

---
+++ src/pages/MarkdownPage.vue
@@ -0,0 +1,31 @@
+<template>
+  <div class="flex w-full flex-wrap">
+    <h1 class="w-full text-center text-3xl my-4">Markdown App</h1>
+    <section class="flex m-auto w-10/12 h-screen">
+      <article class="w-1/2 border">
+        <textarea v-model="text" class="w-full h-full"></textarea>
+      </article>
+      <article class="w-1/2 border bg-gray-100">
+        {{ markedText }}
+      </article>
+    </section>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+import { marked } from 'marked'
+
+export default defineComponent({
+  data() {
+    return {
+      text: '',
+    }
+  },
+  computed: {
+    markedText(): string {
+      return marked(this.text)
+    },
+  },
+})
+</script>
--- src/router.ts
+++ src/router.ts
@@ -2,11 +2,13 @@ import { createRouter, createWebHistory } from 'vue-router'
 import Home from './pages/HomePage.vue'
 import DcHeros from './pages/DcHerosPage.vue'
 import Calendar from './pages/CalendarPage.vue'
+import Markdown from './pages/MarkdownPage.vue'
 
 const routes = [
   { path: '/', component: Home },
   { path: '/dc-heros', component: DcHeros },
   { path: '/calendar', component: Calendar },
+  { path: '/markdown', component: Markdown },
 ]
 
 const router = createRouter({
